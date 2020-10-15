496544b9391f20d2c60dace2bdc1faed24cd6bf3
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-10-15 13:11:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_number = nil
+  smallest_key = nil
+  name_hash.each do |name, num|
+    if  smallest_number == nil || smallest_number > num
+      smallest_number =  num
+      smallest_key = name
+    end
+  end
+  smallest_key
 end
+
+
+# {:blake => 500, :ashley => 2, :adam => 1}
