c50c1f31f4dcab921fc4b13352fc0056a25cd2c6
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-10-15 13:15:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+
+
+#we need to compare to the smallest value but we dont know what that value is, therefore 'smallest_number=nil'
+#smallest_key is the key  of the smallest value which is also not known, therefore 'smallest_key=nil'
+
+
 def key_for_min_value(name_hash)
   smallest_number = nil
   smallest_key = nil
